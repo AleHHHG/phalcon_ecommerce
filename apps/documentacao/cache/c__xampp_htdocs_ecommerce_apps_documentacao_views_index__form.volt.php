af54d4ffb450fde24c67ca8c07166f24c7b12282
AleHHHG <alehguedes@live.com>
2015-08-20 11:50:11 -0300

    Removendo gitignore arquivos

--- apps/documentacao/cache/c__xampp_htdocs_ecommerce_apps_documentacao_views_index__form.volt.php
+++
@@ -1,7 +0,0 @@
- <?php foreach ($form as $element) { ?>
- <div class="form-group">
-     <?php echo $element->label(array('class' => 'form-label')); ?>
-     <?php echo $element; ?>
- </div>
-<?php } ?>
-<?php echo $this->tag->submitButton(array('Salvar', 'class' => 'btn btn-primary btn-lg')); ?>
--- apps/documentacao/views/index/_form.volt
+++
@@ -1,7 +0,0 @@
- {% for element in form %}
- <div class="form-group">
-     {{ element.label(['class': 'form-label']) }}
-     {{ element }}
- </div>
-{% endfor %}
-{{ submit_button('Salvar','class':'btn btn-primary btn-lg') }}
